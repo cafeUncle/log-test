27d4eeb0251199efb22f336fa38001c1dee0c59b
yangyao <yangy_coder@163.com>
2018-05-26 18:28:53 +0800

    config

--- src/main/java/com/example/logtest/LogController.java
+++ src/main/java/com/example/logtest/LogController.java
@@ -1,13 +1,25 @@
 package com.example.logtest;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LogController {
 
+    final static Logger logger = LoggerFactory.getLogger(LogController.class);
+
     @GetMapping("/")
     public String hello() {
+        logger.trace("hello, {}", "/trace");
+        logger.debug("hello, {}", "/debug");
+        logger.info("hello, {}", "/info");
+        logger.warn("hello, {}", "/warn");
+        logger.error("hello, {}", "/error");
+
         return "hello";
     }
 }
